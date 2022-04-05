b62693166d941861d589694ede9399cc645bf8c2
Aoliarachman <aoliarachman150@gmail.com>
2022-04-05 12:13:05 +0700

    first commit

---
+++ go_test01.go
@@ -0,0 +1,11 @@
+package percobaan_1
+
+// nama adalah parameter
+// return untuk tye string
+
+func alamat(Tujuan string) string {
+	return "KE" + Tujuan
+}
+func bagi(a int, b int) int {
+	return a / b
+}
